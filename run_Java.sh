acb44e5ef0550ab5858609743ea348821d0da913
huade <huashi@cisco.com>
2016-05-09 09:22:22 +0800

    add study note

--- run_Java.sh
+++ run_Java.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 
 
-
+#****************************************************
+#    study note
+#  --1) javac
+#       for each .java file, is better to given structure
+#       output path for .class file like ./class/ModuleA(package)
+#        ./class/MoudleB(Package)
+#
+#  --2) java
+#       just run the final main function in the main entry class
+#       need other class file like ./class/MoudleA(Package)
+#                                  ./class/MoudleB(Package)
+#       So need to specialfy the actual denpendency class path
+#       by using option -classpath or -cp, like:
+#       java  -classpath ${ClassDir01}:${ClassDir02}
+#
+#       at this stage, there is no need for the source code (.java)
+#
+# date 2016.05.09
+# huashi@cisco.com
+#***************************************************
 run_Main()
 {
 	OutDir="./out"
